97ec8d49d343f2dc3f2ed380d06086bdfa6b2d9b
Budzserator <borokbora@gmail.com>
2023-03-06 13:20:03 +0100

    Implement Login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+
 class AuthController extends Controller
 {
     public function signup(SignupRequest $request)
@@ -21,24 +22,24 @@ class AuthController extends Controller
             'password' => bcrypt($data['password']),
         ]);
 
-        $token = $user ->createToken('main') -> plainTextToken;
+        $token = $user->createToken('main')->plainTextToken;
 
-        return response(compact('user','token'));
+        return response(compact('user', 'token'));
     }
 
     public function login(LoginRequest $request)
     {
         $credentials = $request->validated();
-        if(Auth::attempt($credentials)){
+        if (!Auth::attempt($credentials)) {
             return response([
                 'message' => 'Provided email address or password is incorrect'
-            ]);
+            ], 422);
         }
         /** @var  User $user */
         $user = Auth::user();
         $token = $user->createToken('main')->plainTextToken;
 
-        return response(compact('user','token'));
+        return response(compact('user', 'token'));
 
     }
 
@@ -47,7 +48,7 @@ class AuthController extends Controller
         /** @var User $user */
         $user = $request->user();
         $user->currentAccessToken()->delete();
-        return response('',204);
+        return response('', 204);
 
     }
 }
