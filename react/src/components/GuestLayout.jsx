7d0c170e8e90d2c08f976ed7bffa9df2ddd2921c
kantm <kantm@DESKTOP-H26FR5O>
2023-03-08 14:40:52 +0100

    Fix when '/' route accessed with token redirects to login

--- react/src/components/GuestLayout.jsx
+++ react/src/components/GuestLayout.jsx
@@ -2,10 +2,12 @@ import {Navigate, Outlet} from "react-router-dom";
 import {useStateContext} from "../contexts/ContextProvider.jsx";
 
 export default function GuestLayout() {
-  const {token} = useStateContext()
+  const {token} = useStateContext();
+
   if (token) {
     return <Navigate to="/"/>
   }
+  
   return (
     <div>
       <Outlet/>
