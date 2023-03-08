903f36b3ef1b25098608823a4c49a7646d364ccd
kantm <kantm@DESKTOP-H26FR5O>
2023-03-08 13:40:50 +0100

    Add extra fields in the signup form

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -8,8 +8,14 @@ export default function Signup() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmationRef = useRef();
+  const ageRef = useRef();
+  const weightRef = useRef();
+  const heightRef = useRef();
+  const sexRef = useRef();
+
   const [errors, setErrors] = useState(null);
   const {setUser, setToken} = useStateContext()
+
   const onSubmit = (ev) => {
     ev.preventDefault()
     const payload = {
@@ -17,6 +23,11 @@ export default function Signup() {
       email: emailRef.current.value,
       password: passwordRef.current.value,
       password_confirmation: passwordConfirmationRef.current.value,
+      age: ageRef.current.value,
+      weight: weightRef.current.value,
+      height: heightRef.current.value,
+      sex: sexRef.current.value,
+      
     }
     axiosClient.post('/signup', payload)
       .then(({data}) => {
@@ -44,6 +55,14 @@ export default function Signup() {
           <input ref={emailRef} type="email" placeholder="Email Address"/>
           <input ref={passwordRef} type="password" placeholder="Password"/>
           <input ref={passwordConfirmationRef} type="password" placeholder="Password Confirmation"/>
+          <input ref={ageRef} type="number" placeholder="Age" />
+          <input ref={heightRef} type="number" placeholder="Height in cm" />
+          <input ref={weightRef} type="number" placeholder="Weight in kg" />
+          <select name="sexes" id="sexes" placeholder="Sex">
+            <option ref={sexRef} value="male">Male</option>
+            <option ref={sexRef} value="male">Female</option>
+            <option ref={sexRef} value="male">Other</option>
+          </select>
           <button className="btn btn-block">Signup</button>
           <p className="message">
             Already Registered? <Link to="/login">Sign in</Link>
