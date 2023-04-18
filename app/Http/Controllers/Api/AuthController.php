ebf66adb8bf3a989edc8197e1d1d1411d1b97367
kantm <kantm@DESKTOP-H26FR5O>
2023-04-18 20:45:47 +0200

    Add index function in AuthController to get user data

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -12,6 +12,13 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+
+    public function index()
+    {
+        $user = auth()->user();
+        return response(compact('user'));
+    }
+
     public function signup(SignupRequest $request)
     {
         /** @var \App\Models\User $user */
